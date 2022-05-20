652244392cff75776e926804cab05cc0cf00b091
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-05-20 06:57:46 +0430

    Update banner.py

--- module/banner.py
+++ module/banner.py
@@ -1,24 +1,24 @@
 import os
 from colorama import Fore
-import time
+from time import sleep
 from subprocess import Popen
+
 def banner():
     os.system("clear")
-    print("")
     Popen('neofetch')
-    time.sleep(2)
+    sleep(2)
 
 def show_menu():
     blue='\033[34m'
-    time.sleep(0.1)
+    sleep(0.1)
     print(Fore.RED+"\n ["+Fore.WHITE+"*"+Fore.RED+"]"+Fore.CYAN+" Choose one of the options below \n")
-    time.sleep(0.1)
+    sleep(0.1)
     print(Fore.RED+" [0]"+blue+" Get Normal Data "+Fore.YELLOW+"[Without Any Permissions]\n")
-    time.sleep(0.1)
+    sleep(0.1)
     print(Fore.RED+" [1]"+blue+" Get Location "+Fore.GREEN+"[SMARTPHONES]\n")
-    time.sleep(0.1)
+    sleep(0.1)
     print(Fore.RED+" [2]"+blue+" Access Webcam\n") 
-    time.sleep(0.1)
+    sleep(0.1)
     print(Fore.RED+" [3]"+blue+" Access Microphone \n")
-    time.sleep(0.1)
+    sleep(0.1)
     print(Fore.RED+" [4]"+blue+" Exit . . .\n")
