dcd1667672bc6ac2f9cb870e9cffcba818b8cbf3
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-06-13 16:01:02 +0430

    Create colors.py

---
+++ module/colors.py
@@ -0,0 +1,21 @@
+# class c is Abbreviations of colors 
+# lcyan or lgreen is Abbreviations of lightcyan || light green
+
+class c:
+  black='\033[30m'
+  red='\033[31m'
+  green='\033[32m' 
+  orange='\033[33m' 
+  blue='\033[34m' 
+  purple='\033[35m' 
+  cyan='\033[36m' 
+  lgrey='\033[37m' 
+  grey='\033[90m'
+  lred='\033[91m' 
+  lgreen='\033[92m'
+  yellow='\033[93m' 
+  lblue='\033[94m' 
+  pink='\033[95m' 
+  lcyan='\033[96m'
+  re='\033[0m'
+  
