289bc41816384e4e0c5e61c6932e9077bf399f46
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-06-17 19:48:34 +0430

    Update messages.py

--- module/messages.py
+++ module/messages.py
@@ -1,11 +1,12 @@
 from module.colors import c
+
+#Message for After_click function 
 def show_message(msg):
     if msg == "loc":
-        print(c.red+"\nWaiting for User Interaction ! ! !\n")
+        print(c.yellow+"\n[ - ] "+c.red+"Waiting for User Interaction ! ! !\n")
     
     elif msg == "mic":
-        print(c.red+"\nWaiting to receive victim Voice ! ! ! \n")
+        print(c.yellow+"\n[ - ] "+c.red+"\nWaiting to receive victim Voice ! ! ! \n")
 
     elif msg == "webcam":
-        print(c.red+"\nWaiting to receive victim Picture ! ! !\n")
-    
+        print(c.yellow+"\n[ - ] "+c.red+"Waiting to receive victim Picture ! ! !\n")
