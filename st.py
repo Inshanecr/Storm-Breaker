e8554d94066d62d3da28237ac9bfd3e2504ed075
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-06-17 19:42:06 +0430

    Update st.py

--- st.py
+++ st.py
@@ -1,41 +1,37 @@
-import subprocess
 from module import check
+
+#check Some installation And Dependencis
 check.dependency()
+check.check_update()
+check.check_started()
 from module.colors import c
-from module import handler,localhost,banner
+from module import handler,localhost,banner,control
+
 while True:
     banner.banner()
     banner.show_menu() 
 
     try:
         User = input(c.red+" ┌─["+c.lcyan+"STORM-BREAKER"+c.re+"~"+c.lgreen+"@HOME"+c.red+"""]
- └──╼ """+c.green+"$ ")
-        if  User == "1":
-           banner.banner()
-           handler.normal_data()
-        elif User == "2":
+ └──╼ """+c.green+"$ "+c.pink)
+        if User == "1":
            banner.banner()
            handler.weather_info()
-        elif User == "3":
+        elif User == "2":
            banner.banner()
            handler.nearyou_info()
-        elif User == "4":
+        elif User == "3":
            banner.banner()
            handler.WebCamInfo()
-        elif User == "5":
+        elif User == "4":
            banner.banner()          
            handler.MicrophoneInfo()
-        elif User == "6":
+        elif User == "5":
            banner.Devloper_information()
-        elif User == "7":        
-            localhost.kill()
+        elif User == "6":        
+            control.kill_process()
             exit(c.lred+"\n Goodbye :) ")
-        
 
-    except Exception as e: 
-       print(e)
-       sleep(5)
-       localhost.kill()   
     except KeyboardInterrupt:
-      localhost.kill()
+      control.kill_process()
       exit(c.cyan+"\n Goodbye :)")
