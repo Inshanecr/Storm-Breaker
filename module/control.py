260b9e6316f4b39797ed572327120619e418638a
VCH <92458450+Inshanecr@users.noreply.github.com>
2022-06-17 19:46:23 +0430

    Update control.py

--- module/control.py
+++ module/control.py
@@ -1,22 +1,20 @@
 import json,subprocess
 
-def kill_php_proc():
-    with open("Settings.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-        pid = data["pid"]
 
+#Kill php localHost
+def kill_process():
     try:
-        for i in pid:
-            subprocess.getoutput(f"kill -9 {i}")
-
-
-        
-        else:
-            pid.clear()
-            data["pid"] = []
-            with open("Settings.json", "w") as jsonFile:
-                json.dump(data, jsonFile)
-
-    except:
-        pass
-
+     with open( 'Settings.json','r') as pidLog:
+       data = json.load(pidLog)
+       pid = data["pid"]
+       for i in pid:
+             subprocess.getoutput(f"killall -9 {i}")
+          
+       else:
+             pid.clear()
+             data["pid"] = []
+             with open("Settings.json","w") as logs:
+                json.dump(data,logs)
+                          
+    except Exception as e:
+         exit(e)
